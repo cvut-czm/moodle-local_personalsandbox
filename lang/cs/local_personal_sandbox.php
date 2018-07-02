d69ad50b2598e2df963e833cc70db46b1ded338e
Jiří Fryč <jirifryc.cz@jirifryc.cz>
2018-07-02 12:16:44 +0200

    :globe_with_meridians: Adding Czech translation.

---
+++ lang/cs/local_personal_sandbox.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by CTU CZM.
+ * Author: Jiri Fryc
+ * License: GNU GPLv3
+ */
+
+$string['pluginname']='Personal sandbox';
+$string['my_sandbox']='Můj sandbox';
+$string['form_create_new:text']='Zatím nemáte vlastní sandbox. Chcete ho vytvořit?';
+$string['form_create_new:title']='Personal sandbox';
+
+/*
+ * Settings
+ */
+$string['settings:show_in_sidebar']='Zobrazit odkaz v sidebar';
+$string['settings:show_in_sidebar_desc']='Pokud toto nechcete, tak můžete odkudkoliv v aplikaci přesměrovat na: <pre><code>/local/personal_sandbox/my.php</code></pre>';
+$string['settings:remove_after_duration']='Odstranit sandbox po';
+$string['settings:remove_after_duration_desc']='Po tomto čase bude sandbox automaticky odstraněn. <pre><code>Nastavte 0 pro nikdy.</code></pre>';
+$string['settings:user_role']='Role uživatele';
+$string['settings:user_role_desc']='Která role má být přiřazena uživateli.';
+$string['settings:change_name']='Měnitelný název kurzu';
+$string['settings:change_name_desc']='Jestli majitel sandboxu může měnit název kurzu';
+$string['settings:change_visibility']='Měnitelná viditelnost kurzu';
+$string['settings:change_visibility_desc']='Jestli majitel sandboxu může měnit viditelnost kurzu.';
+$string['settings:header1']='Personal sandbox';
+
+
+/*
+ * Tasks
+ */
+$string['task:clear_old_task_name']='Čištění starých sandboxů';
+
+
+/*
+ * Notifications
+ */
+$string['notification:course_name_change_prevent']='Nemáte povoleno měnit název sandboxu.';
+$string['notification:course_visibility_change_prevent']='Nemáte povoleno měnit viditelnost sandboxu.';
+$string['notification:change_prevent_desc']='Změnili jsme to zpět. Všechny ostatní nastavení byly úspěšně uloženy.';
+
+/*
+ * Privacy API
+ */
+$string['privacy:metadata:course_personal_sandbox']='Personal sandbox plugin využívá použe ID uživatele k mapování.';
+$string['privacy:metadata:course_personal_sandbox:userid']='ID uživatele se využívá k jeho mapování k sandboxu.';
